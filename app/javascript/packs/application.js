259b097c9c8fe38cd6c427f1dc7e2ad972a528b1
谷村有寿麻 <t.azumax0502@gmail.com>
2021-04-16 22:47:19 +0900

    jquery導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,8 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+import $ from 'jquery'
+import axios from 'axios'
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -15,3 +17,15 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+document.addEventListener('DOMContentLoaded', () => {
+  $('.profile-image').on('click', () => {
+    $('.profile-image-modal').fadeIn()
+    $('.profile-image-modal').on('click', () => {
+      $('.profile-image-modal').fadeOut()
+    })
+    $('#profile-modal-close').on('click', () => {
+      $('.profile-image-modal').fadeOut()
+    })
+  })
+})
